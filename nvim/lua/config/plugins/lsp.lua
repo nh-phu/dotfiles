ddb44ea6d178914f80f78b5b643766570d580d58
HaiPhu166 <nguyenhaiphu1662006@gmail.com>
2025-06-09 00:31:11 +0700

    Update Neovim config
    add LSP settings

--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -1,57 +1,169 @@
-
 return {
+  -- Mason (LSP installer) - should come first
+  { "williamboman/mason.nvim", build = ":MasonUpdate", config = true },
+  { 
+    "williamboman/mason-lspconfig.nvim", 
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",        -- Lua
+          "clangd",        -- C/C++
+          "pyright",       -- Python (or "pylsp" for alternative)
+        },
+        automatic_installation = true,
+      })
+    end 
+  },
 
   -- LSP Setup (language servers)
-  { "neovim/nvim-lspconfig", config = function()
-    require("lspconfig").lua_ls.setup({
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" }, -- Recognize 'vim' as a global variable
-                },
-                workspace = {
-                  library = vim.api.nvim_get_runtime_file("", true), -- Include Neovim runtime files
-                },
-              },
+  { 
+    "neovim/nvim-lspconfig", 
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      
+      -- Lua LSP
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
             },
-        })
-  end },
-
-  -- Mason (LSP installer)
-  { "williamboman/mason.nvim", build = ":MasonUpdate", config = true },
-  { "williamboman/mason-lspconfig.nvim", config = true },
-
-  -- Autocompletion
-{  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "saadparwaiz1/cmp_luasnip",
-    "L3MON4D3/LuaSnip",
-  },
-  config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      })
+      
+      -- C/C++ LSP (clangd)
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+          "--function-arg-placeholders",
+          "--fallback-style=llvm",
+        },
+        init_options = {
+          usePlaceholders = true,
+          completeUnimported = true,
+          clangdFileStatus = true,
+        },
+      })
+      
+      -- Python LSP (pyright)
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              diagnosticMode = "workspace",
+              useLibraryCodeForTypes = true,
+            },
+          },
+        },
+      })
+      
+      -- Global LSP keymaps
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          local opts = { buffer = ev.buf }
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', '<leader>f', function()
+            vim.lsp.buf.format { async = true }
+          end, opts)
         end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<Tab>"] = cmp.mapping.select_next_item(),
-        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-      }),
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "buffer" },
-        { name = "path" },
-        { name = "luasnip" },
-      },
-    })
-  end,
-},
+      })
+    end 
+  },
+
+  -- Autocompletion with Copilot integration
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
+      "zbirenbaum/copilot-cmp", -- Copilot integration for nvim-cmp
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<CR>"] = cmp.mapping.confirm({ 
+            select = true,
+            behavior = cmp.ConfirmBehavior.Replace,
+          }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+        }),
+        sources = cmp.config.sources({
+          { name = "copilot", priority = 1000 },
+          { name = "nvim_lsp", priority = 900 },
+          { name = "luasnip", priority = 800 },
+        }, {
+          { name = "buffer", priority = 700 },
+          { name = "path", priority = 600 },
+        }),
+        formatting = {
+          format = function(entry, vim_item)
+            -- Add source indicators
+            vim_item.menu = ({
+              copilot = "[Copilot]",
+              nvim_lsp = "[LSP]",
+              luasnip = "[Snippet]",
+              buffer = "[Buffer]",
+              path = "[Path]",
+            })[entry.source.name]
+            return vim_item
+          end,
+        },
+      })
+    end,
+  },
 }
