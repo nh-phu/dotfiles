9df397e32c62e9b2d697fc677c47cbf579bc4a59
HaiPhu166 <nguyenhaiphu1662006@gmail.com>
2025-09-17 22:41:49 +0700

    Move tree-sitter-context to treesitter.lua

--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -17,6 +17,7 @@ return {
           "cpp",
           "editorconfig",
           "diff",
+          "java",
           "jsdoc",
           "json",
           "jsonc",
@@ -35,8 +36,40 @@ return {
       })
     end,
   },
-
   {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "VeryLazy",
+    opts = function()
+      local tsc = require("treesitter-context")
+      Snacks.toggle({
+        name = "Treesitter Context",
+        get = tsc.enabled,
+        set = function(state)
+          if state then
+            tsc.enable()
+          else
+            tsc.disable()
+          end
+        end,
+      }):map("<leader>ct")
+      return {
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        multiwindow = true, -- Enable multiwindow support.
+        max_lines = 3, -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 20, -- Maximum number of lines to show for a single context
+        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+        -- Separator between context and content. Should be a single character string, like '-'.
+        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+        separator = '-',
+        zindex = 20, -- The Z-index of the context window
+        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+      }
+    end,
+  },
+  --[[ {
     "RRethy/vim-illuminate",
     config = function()
       -- Configure vim-illuminate
@@ -53,5 +86,5 @@ return {
         },
       })
     end,
-  },
+  }, ]]
 }
