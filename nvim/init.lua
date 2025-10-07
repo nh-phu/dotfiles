6ccc23510b6dabfcf226f29756eef6fc11532d0d
HaiPhu166 <nguyenhaiphu1662006@gmail.com>
2025-10-07 11:23:32 +0700

    Neovide config

--- nvim/init.lua
+++ nvim/init.lua
@@ -164,3 +164,11 @@ vim.diagnostic.config({
   --  current_line = true,
   -- },
 })
+if vim.g.neovide then
+    vim.g.neovide_confirm_quit = true
+    vim.g.neovide_cursor_trail_size = 1.0
+    vim.g.neovide_cursor_vfx_mode = ""
+    vim.g.neovide_cursor_animation_length = 0
+    vim.g.neovide_scroll_animation_length = 0
+    vim.g.neovide_hide_mouse_when_typing = true -- optional, feels snappier
+end
